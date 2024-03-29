d604b8059683de6b5b39c140b8fe0fdec35154ad
zxBAI Garcia,Juan_Manuel (IT INF) EXTERNAL <juan_manuel.garcia.ext@boehringer-ingelheim.com>
2024-03-29 01:38:51 -0300

    [MOD] dding modal views and adding CreateHistory.cshtml

---
+++ MVC/Controllers/HistoriaController.cs
@@ -0,0 +1,25 @@
+﻿using Entidades.Models;
+using Microsoft.AspNetCore.Mvc;
+using Servicios;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MVC.Controllers
+{
+    public class HistoriaController : Controller
+    {
+        private IHistoriaService _historiaService;
+        private IPacienteService _pacienteService;
+
+        public HistoriaController(IHistoriaService historiaService, IPacienteService pacienteService)
+        {
+            _historiaService = historiaService;
+            _pacienteService = pacienteService;
+
+        }
+        public ActionResult CreateHistory(int id)
+        {
+            ViewBag.Paciente = _pacienteService.getPatient(id);
+            return View();
+        }
+    }
+}
--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1968b372431f7195349dc4ab87967677c7dbf42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175d191c7edf65e8f0d814f8b27a5753e9511413")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
