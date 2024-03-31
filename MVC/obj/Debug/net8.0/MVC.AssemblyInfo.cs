44fe4d03baaec77f4cb8463587f45f79b18c7056
zxBAI Garcia,Juan_Manuel (IT INF) EXTERNAL <juan_manuel.garcia.ext@boehringer-ingelheim.com>
2024-03-31 01:15:08 -0300

    [MOD] adding search input in ListPatients.cshtml

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c14bee75cc6a923ad21dca43cd86dd4d383e764")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad1dfd2a65006cdd60711341e2cf08b1f7edc81")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MVC/wwwroot/js/site.js
+++ MVC/wwwroot/js/site.js
@@ -3,5 +3,26 @@
 
 // Write your JavaScript code.
 
+function filter_patientes() {
+    var input, filter, table, tr, td, i, j, visible;
+    input = document.querySelector("#input_data");
+    filter = input.value.toUpperCase();
+    table = document.querySelector("#active_patients");
+    tr = table.querySelectorAll("tr");
+    for (i = 1; i < tr.length; i++) {
+        visible = false;
+        td = tr[i].querySelectorAll("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j] && td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                visible = true;
+            }
+        }
+        if (visible === true) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
 
 
