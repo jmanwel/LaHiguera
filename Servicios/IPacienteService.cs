884036abd8bfa453deebfa863fc10d17c2c8b0b4
guido.pardo@boehringer-ingelheim.com <guidopardo@gmail.com>
2024-04-09 00:09:06 -0300

    New list for inactive patients was created

--- Servicios/IPacienteService.cs
+++ Servicios/IPacienteService.cs
@@ -6,7 +6,9 @@ namespace Servicios
     public interface IPacienteService
     {
         List<Paciente> getAllPatients();
+        List<Paciente> getAllInactivePatients();
         void create(Paciente paciente);
+        void setActivate(int id_patient);
         void setDeactivate(int id_patient);
         Paciente getPatient(int id_patient);
         void editPatient(Paciente paciente);
