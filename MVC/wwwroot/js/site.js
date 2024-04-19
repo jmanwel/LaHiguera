d287c38f96f218f64d7041a5d041a405d5a9dbc7
zxBAI Garcia,Juan_Manuel (IT INF) EXTERNAL <juan_manuel.garcia.ext@boehringer-ingelheim.com>
2024-04-19 16:12:35 -0300

    [MOD] site.js

--- MVC/wwwroot/js/site.js
+++ MVC/wwwroot/js/site.js
@@ -49,3 +49,21 @@ $(document).ready( function () {
 } );
 
 
+function calcula_imc_pediatria() {
+    let talla = document.querySelector("#pediatria_talla").value;
+    let peso = document.querySelector("#pediatria_peso").value;
+    if (talla == "" || peso == "") {
+        alert("Por favor, complete los campos talla/peso");
+        document.querySelector("#pediatria_peso").focus();
+    } else {
+        let imc = document.querySelector("#pediatria_imc");
+        talla = parseFloat(document.querySelector("#pediatria_talla").value);
+        peso = parseFloat(document.querySelector("#pediatria_peso").value);
+        if (talla == 0) {
+            alert("Talla debe ser mayor a 0");
+            document.querySelector("#pediatria_talla").focus();
+        } else {
+            imc.value = peso / Math.pow((talla / 100), 2)
+        }
+    }
+}
