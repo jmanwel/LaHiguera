5822459d5071473e01ae195c81d7bedc64348f93
zxBAI Garcia,Juan_Manuel (IT INF) EXTERNAL <juan_manuel.garcia.ext@boehringer-ingelheim.com>
2024-08-13 15:09:51 -0300

    [MOD] HistoriaService.cs | Historia.cs

--- Entidades/Models/Historia.cs
+++ Entidades/Models/Historia.cs
@@ -63,7 +63,7 @@ public partial class Historia
 
     public string? ObservacionDeriv { get; set; }
 
-    public DateTime? FechaCreacion { get; set; }
+    public DateOnly? FechaCreacion { get; set; }
 
-    public DateTime? LastUpdated { get; set; }
+    public DateOnly? LastUpdated { get; set; }
 }
