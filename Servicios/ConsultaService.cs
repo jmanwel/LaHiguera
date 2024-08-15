f3d839fde49f55e01c4d326186fdf797386331a4
zxBAI Garcia,Juan_Manuel (IT INF) EXTERNAL <juan_manuel.garcia.ext@boehringer-ingelheim.com>
2024-08-15 10:43:43 -0300

    [MOD] ConsultaService.cs

--- Servicios/ConsultaService.cs
+++ Servicios/ConsultaService.cs
@@ -19,7 +19,7 @@ namespace Servicios
         {
             //This method persists Consulta objects in DDBB
             try {
-                consulta.FechaCreacion = DateOnly.FromDateTime(DateTime.Now);
+                consulta.FechaCreacion = DateTime.Now;
                 //Paso a uppercase los campos de texto antes de guardarlo
                 consulta.MotivoConsulta = consulta.MotivoConsulta.ToUpper();
                 consulta.DiagnosticoConsulta = consulta.DiagnosticoConsulta?.ToUpper() ?? "";
@@ -60,7 +60,7 @@ namespace Servicios
                 edited_consultation.Fum = consulta.Fum;
                 edited_consultation.MacActual = consulta.MacActual?.ToUpper() ?? "";
                 edited_consultation.PacienteId = consulta.PacienteId;
-                edited_consultation.LastUpdated= DateOnly.FromDateTime(DateTime.Now);
+                edited_consultation.LastUpdated= DateTime.Now;
                 _ctxt.SaveChanges();
                 Console.WriteLine("Consulta modificada OK!");
             }
