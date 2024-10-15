1c35aaded36930eedb10d8fb0279573271b8e009
guido.pardo@boehringer-ingelheim.com <guidopardo@gmail.com>
2024-10-15 08:25:29 -0300

    Color tags for Antecedentes

---
+++ Entidades/Models/AntecedenteView.cs
@@ -0,0 +1,26 @@
+namespace Entidades.Models;
+
+public class AntecedenteView
+{
+    public int? PacienteId { get; set; }
+    public string? Sedentarismo { get; set; }
+    public string? Alcohol { get; set; }
+    public string? Drogas { get; set; }
+    public string? Tabaco { get; set; }
+    public string? Alergias { get; set; }
+    public string? Diabetes { get; set; }
+    public string? Hta { get; set; }
+    public string? Dislipemia { get; set; }
+    public string? Obesidad { get; set; }
+    public string? Chagas { get; set; }
+    public string? Hidatidosis { get; set; }
+    public string? Tbc { get; set; }
+    public string? Cancer { get; set; }
+    public string? Quirurgicos { get; set; }
+    public string? RiesgoNutricional { get; set; }
+    public string? RiesgoSocial { get; set; }
+    public string? Personales { get; set; }
+    public string? Familiares { get; set; }
+    public string? Hospitalizaciones { get; set; }
+    public string? Perinatales { get; set; }
+}
